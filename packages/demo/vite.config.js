978db9993c76de180fbebe0fbfd2f9cdcfa8207c
Jaime Giraldo <sposmen@gmail.com>
2026-05-11 23:12:22 -0700

    Updated GH P Title

--- packages/demo/vite.config.js
+++ packages/demo/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/json_dynamic_viewer/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       'json-dynamic-viewer': resolve(__dirname, '../lib/src/index.js'),
     },
   },
-});
+}));
